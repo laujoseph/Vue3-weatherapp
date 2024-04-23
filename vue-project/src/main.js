54139486c300cde7846cb7f4f3a2b2c771eddb55
Joseph <jojojosephlau@gmail.com>
2024-04-24 02:02:36 +0800

    Added NavBar and Modal for Instructions

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/tailwindcss'
+import './assets/tailwind.css'
 
 const app = createApp(App)
 
